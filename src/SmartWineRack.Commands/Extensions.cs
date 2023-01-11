fd7217a9a597fcb23c8801665d35de70d86c62ec
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2023-01-10 19:50:28 -0800

    Stylecop

--- src/SmartWineRack.Commands/Extensions.cs
+++ src/SmartWineRack.Commands/Extensions.cs
@@ -4,8 +4,17 @@
 
 namespace SmartWineRack.Commands
 {
+    /// <summary>
+    /// Extension methods.
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Ensures the object is not null by throwing an exception if it is.
+        /// </summary>
+        /// <param name="o">The object to test for null.</param>
+        /// <param name="paramName">The parameter name that appears in the exception message.</param>
+        /// <exception cref="ArgumentNullException">The exception thrown if the object is null.</exception>
         public static void EnsureNotNull(this object? o, string paramName)
         {
             if (o == null)
@@ -14,6 +23,12 @@ namespace SmartWineRack.Commands
             }
         }
 
+        /// <summary>
+        /// Ensures the given key exists in the dictionary by throwing an exception if the key does not exist.
+        /// </summary>
+        /// <param name="dictionary">The dictionary.</param>
+        /// <param name="key">The key to test.</param>
+        /// <exception cref="InvalidOperationException">The exception thrown if the key does not exist.</exception>
         public static void EnsureKey(this IDictionary<string, object> dictionary, string key)
         {
             if (!dictionary.ContainsKey(key))
